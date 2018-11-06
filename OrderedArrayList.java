f8fa13bf98549b2cff3301e496fb55f54c00398a
ylee02 <ylee01@stuy.edu>
2018-11-07 03:12:39 +0900

    UpdatedFIle

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,5 +8,41 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
+  public void add(int index, T element) {
+	  if (this.size() == 0) {
+		  super.add(element);
+	  }
+	  else if (this.size() == 1){
+		  if (this.get(0).compareTo(element) >= 0) {
+			  super.add(element);
+		  }
+		  else {
+			  super.add(0,element);
+		  }
+	  }
+	  else {
+		  boolean test = true;
+		  int i = 0;
+		  while (test) {
+			  if (element.compareTo(this.get(i)) >= 0) {
+				  super.add(i, element);
+				  test = false;
+			  }
+			  i += 1;
+		  }
+	  }
+  }
   
+  public boolean add(T element){
+	  this.add(0,element);
+	  return true;
+  }
+  
+  public T set(int index, T element) {
+	  remove(index);
+	  this.add(element);
+	  return element;
+  }
+	  
+		  
 }
